b4b7c0c9176803f9091d703bc93cbbd0605ed396
Wurielle <noahfenghom@gmail.com>
2021-10-18 23:34:04 +0200

    fix: another build fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -77,10 +77,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String,
   },
